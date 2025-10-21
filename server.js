8201670f9020e1fcbfd61ab0668e6b0aa77703d0
Arasu-7 <malarasu@capecomsolutions.com>
2025-10-21 17:10:17 +0530

    added cors

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: true,       // allow all origins
+    credentials: true,  // allow cookies to be sent
+  })
+);
 
 app.use("/api/auth", authRoutes);
 
@@ -15,10 +23,10 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.error("MongoDB connection error:", err));
+  .catch((err) => console.error("MongoDB connection error:", err));
